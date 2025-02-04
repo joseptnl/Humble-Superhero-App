edfe7f9ca657feabf1c8a4a658fea7f33918e67a
Josep <joseptnl@gmail.com>
2025-02-04 22:56:40 +0100

    Configure tailwind as exposed for v4

--- web-client/tailwind.config.js
+++ web-client/tailwind.config.js
@@ -1,5 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {
       colors: {
@@ -12,12 +14,17 @@ module.exports = {
         muted: "#CBD5E0",
       },
       fontFamily: {
-        sans: ['"Inter"', "sans-serif"],
+        sans: ['Inter"', "sans-serif"],
       },
       boxShadow: {
         cool: "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)",
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
